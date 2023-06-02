74c5b669592e98bb534e988750be0a61ecb33a13
George <george.sburatura@gmail.com>
2023-06-02 16:52:23 +0300

    read the signal and normalize at 220500 samples

--- app/src/main/java/com/example/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -2,16 +2,9 @@ package com.example.audiorecorder;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.media.MediaCodec;
-import android.media.MediaExtractor;
-import android.media.MediaFormat;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,20 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import com.example.audiorecorder.ml.ModelEsc50;
-
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
-
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.ShortBuffer;
-import java.util.Arrays;
 import java.util.Random;
+
+import com.example.audiorecorder.AudioUtils;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private MediaRecorder recorder = null;
     private MediaPlayer player = null;
 
+
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private final String[] permissions = {Manifest.permission.RECORD_AUDIO};
@@ -53,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private int count = 0;
 
     private static final int DATA_SAMPLE_AVERAGE = 220500;
+
+    private short[] amplitudes;
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -98,9 +84,9 @@ public class MainActivity extends AppCompatActivity {
     private void startRecording() {
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.OGG);
         recorder.setOutputFile(fileName);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.OPUS);
 
         try {
             recorder.prepare();
@@ -120,20 +106,12 @@ public class MainActivity extends AppCompatActivity {
         Random rand = new Random();
         int randNum = rand.nextInt(10000);
         String randNumString = String.valueOf(randNum);
-        String randomFileNamePath = "/audiorecordtest" + randNumString + ".3gp";
+        String randomFileNamePath = "/audiorecordtest" + randNumString + ".ogg";
 
         // Record to the external cache directory for visibility
         fileName = getExternalCacheDir().getAbsolutePath();
         fileName += randomFileNamePath;
-    }
 
-    private byte[] readSoundFile(File file) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream(file);
-        int fileSize = (int) file.length();
-        byte[] soundData = new byte[fileSize];
-        fileInputStream.read(soundData);
-        fileInputStream.close();
-        return soundData;
     }
 
 
@@ -192,33 +170,27 @@ public class MainActivity extends AppCompatActivity {
                 count++;
                 classifyMsg = "Test " + count + ": " + fileName;
 
-                try {
-                    File file = new File(fileName);
-                    byte[] soundData = readSoundFile(file);
 
-                    StringBuilder data = new StringBuilder();
-                    for (byte soundDatum : soundData) {
-                        String temp = String.valueOf(soundDatum) + " ";
-                        data.append(temp);
-                    }
+                short[] ampl = AudioUtils.getAmplitudes(fileName);
+                amplitudes = AudioUtils.resizeAmplitudes(ampl, DATA_SAMPLE_AVERAGE);
 
-                    textView.setText(String.valueOf(soundData.length));
-                    textView.append("\n");
-                    textView.append(data);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                StringBuilder data = new StringBuilder();
+                for (short soundDatum : amplitudes) {
+                    String temp = soundDatum + " ";
+                    data.append(temp);
                 }
-                
+
+                String text = fileName + " " + amplitudes.length + "\n";
+                textView.setText(text);
+                textView.append(data);
+
             }
         });
 
 
-        // integrare model tflite sample code
-
 //        try {
-//            ModelEsc50 model = ModelEsc50.newInstance(this); // context se poate inlocui cu this sau modelul respectiv
-//
+//            ModelEsc50 model = ModelEsc50.newInstance(this);
+
 //            // Creates inputs for reference.
 //            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 11, 220, 1}, DataType.FLOAT32);
 //            ByteBuffer byteBuffer = null;
